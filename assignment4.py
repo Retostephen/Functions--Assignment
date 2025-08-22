b81430126ad39383859771c2e6c27ba05911d042
Retostephen <retostephen2006@gmail.com>
2025-08-22 17:45:55 +0100

    Finished Assignment 4

---
+++ assignment4.py
@@ -0,0 +1,6 @@
+def triangle_area(base, height):
+	return 0.5 * base * height
+
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+print(f"Area of triangle is: {(triangle_area(base, height))}")
